dbebacd8edf5ee36e249480afdc5ede056951fb1
priyanshupande <priyanshupande23@gmail.com>
2024-12-08 21:38:35 +0530

    correaction in aap.jsx

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React from "react";
 import ComA from "./ComA";
+// eslint-disable-next-line no-unused-vars
+import App from './App';
+
 
 function App() {
   return (
     <div className="App">
       <ComA />
+
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,8 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
-import ComA from './ComA';
+import ComA from './ComA'; // Keep only the necessary imports
 
 // Create a root using the createRoot API
 const root = ReactDOM.createRoot(document.getElementById("root"));
